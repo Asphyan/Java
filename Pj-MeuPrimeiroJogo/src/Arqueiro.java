aa3208511688578ceeeab6f4cf6934cc6ff0ed93
DevGuiBan <guibandeira290@gmail.com>
2024-06-18 19:40:14 -0300

    delets

--- Pj-MeuPrimeiroJogo/src/Arqueiro.java
+++
@@ -1,8 +0,0 @@
-//'extends' usado para herdar os atributos da classe personagem
-
-public class Arqueiro extends Personagem{
-
-    public Arqueiro(String nome, double vida, String descricao) {
-        super(nome, vida, descricao);
-    }
-}
--- Pj-MeuPrimeiroJogo/src/Mago.java
+++
@@ -1,8 +0,0 @@
-//'extends' usado para herdar os atributos da classe personagem
-
-public class Mago extends Personagem{
-
-    public Mago(String nome, double vida, String descricao){
-        super(nome, vida, descricao);
-    }
-}
